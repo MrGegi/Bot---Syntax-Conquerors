a1c0137604e029eeb8ff57ba7bff8aa328daecda
MrGegi <145406430+MrGegi@users.noreply.github.com>
2024-02-27 10:27:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
 # Bot - Syntax Conquerors
+
+class AddressBook():
+
+class Contact():
+    
+class Name():
+class Phone():
+class Address():
+class Email():
+class Birthday():
+
+
+class Notebook():
+    
+class Note():
+class Tag():
+
+
+def documentation():
+    """Function Description
+    Lets keep documenation up to date.
+    """
+    my_code = 0 # remember to comment important parts of your code
+    return 'good luck and have fun'
+
+
+def main():
+    print('')
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+# OPERATIONS = {
+#     'accepted_commands':accepted_commands,
+#     'hello': hello,
+#     'create_contact': create_contact,
+#     'add_phone': add_phone,
+#     'change_phone_num': change_phone_num,
+#     'show_contact': show_contact,
+#     'delete_phone': delete_phone,
+#     'set_birthday' : set_birthday,
+#     'days_to_birthday': days_to_birthday,
+#     'iterator': iterator,
+#     'show_all': show_all,
+#     'find_contact' : find_contact,
+#     'good_bye': end_program, 
+#     'close': end_program, 
+#     'exit': end_program, 
+#     '.': end_program, 
+# }
