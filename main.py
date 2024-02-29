1af51d14b69b0a01b017948b60d3affedb05b8e6
MrGegi <145406430+MrGegi@users.noreply.github.com>
2024-02-29 12:33:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 from collections import UserDict
-<<<<<<< Updated upstream
-# Bot - Syntax Conquerors
-=======
 import re
->>>>>>> Stashed changes
-
-# OPERATIONS = {
-#     'accepted_commands':accepted_commands,
-#     'hello': hello,
-#     'create_contact': create_contact,
-#     'add_phone': add_phone,
-#     'change_phone_num': change_phone_num,
-#     'show_contact': show_contact,
-#     'delete_phone': delete_phone,
-#     'set_birthday' : set_birthday,
-#     'days_to_birthday': days_to_birthday,
-#     'iterator': iterator,
-#     'show_all': show_all,
-#     'find_contact' : find_contact,
-#     'good_bye': end_program, 
-#     'close': end_program, 
-#     'exit': end_program, 
-#     '.': end_program, 
-# }
 
 class AddressBook(UserDict):
     def __init__(self):
         self.contacts = {}
 
     def add_contact(self, name, last_name):
-        address_book.contacts[name + ' ' + last_name] = Contact(name, last_name)
+        self.contacts[name + ' ' + last_name] = Contact(name, last_name)
 
 class Contact():
     def __init__(self, name, last_name):
         self.name = Name(name)
         self.last_name = Name(last_name)
+        self.address = ''
+        self.note = ''
 
-class Field: # Parent
+    def add_address(self, address):
+        self.address = Address(address).value
+
+    def remove_address(self):
+        self.address = ''
+
+    def change_address(self, address):
+        self.address = Address(address).value
+
+
+    def add_notebook(self, note):
+        self.note = Notebook(note).value
+
+    def remove_notebook(self):
+        self.note = ''
+
+    def change_notebook(self, note):
+        self.note = Notebook(note).value
+
+class Field:
     """
     Można się teraz odnieść do settera za pomocą @Field.value.setter.
     Należy tylko do klasy dziedziczącej (child) w nawiasie nazwę klasy przekazującej (parent).
@@ -45,36 +43,75 @@ class Field: # Parent
     def __init__(self, input_value = None):
         self.value = input_value
 
-    @property # # Dzieki temu możesz używać dodatkowych funkcj dekoratora klasy takich jak .setter ponizej. Dlatego - makes life so much easier :)
+    @property
     def value(self):
         return self.internal_value
+    """
+    Dzieki @property można używać dodatkowych funkcj dekoratora klasy takich jak .setter ponizej.
+    """
     
-    @value.setter # Można się odwwołaać do tego settera w swojej klasie za pomocą @Field.value.setter i zrobić overide.
+    @value.setter
     def value(self, input_value):
         self.internal_value = input_value
+    """
+    Można się odwwołaać do tego settera w swojej klasie za pomocą @Field.value.setter i zrobić overide.
+    """
 
-class Name(Field): # Przykład jak można użyć dekoratora @property do wprowadzenia warunków do settera dla Name.
-    
+class Name(Field):
+    """
+    Przykład jak można użyć dekoratora @property do wprowadzenia warunków do settera dla Name.
+    """
     @Field.value.setter
     def value (self, name):
         if not name:
-            raise ValueError("class_Name-def_value:name_cannot_be_empty") # Ta wiadomość idzie do @input_error Jeśli funkcja handler używa tego gettera do wysłania wartości do obiektu.
+            raise ValueError("class_Name-def_value:name_cannot_be_empty")
+        """
+        Ta wiadomość idzie do @input_error Jeśli funkcja handler używa tego settera do wysłania wartości do obiektu.
+        """
         self.internal_value = name
 
 class Phone():
     pass
 
-class Address():
-    pass
+class Address(Field):
+    @Field.value.setter
+    def value(self, address: str):
+        self.internal_value = address
 
-class Email():
-    pass
+class Email(Field):
+    
+    def __init__(self, email=''):
+       self.__name = None
+       self.email = email
+
+    @property
+    def email(self):
+        return self.__name
+    
+    @email.setter
+    def email(self, email):
+        
+        """Sprawdzenie czy format maila jest prawidłowy"""
+
+        patern_email = r"^([A-Za-z0-9]+ |[A-Za-z0-9][A-Za-z0-9\.\_]+[A-Za-z0-9])@([A-Za-z0-9]+|[A-Za-z0-9\_\-]+[A-Za-z0-9])\.([a-z]{,3}|[a-z]{3}\.[a-z]{2})$"
+        result = re.findall(patern_email,email)
+
+        if result != []:
+            end_text = 'Adress mail has correct format.'
+            self.__name  = email
+        else:
+            end_text = "Wrong mail format!"
+        print(end_text)
+        # print(result)
+        return
 
 class Birthday():
     pass
 
-class Notebook():
-    pass
+class Notebook(Field):
+    @Field.value.setter
+    def value(self, note: str):
+        self.internal_value = note
     
 class Note():
     pass
@@ -83,15 +120,6 @@ class Tag():
     pass
 
 address_book = AddressBook()
-<<<<<<< Updated upstream
-def changelog():
-    """
-    Need somewhere to keep up with the changes.
-    """
-    pass
-
-def test_contacts():  
-=======
 
 def input_error(func):
     """
@@ -107,14 +135,10 @@ def input_error(func):
     return gracefull_error_handling
 
 def test_contacts(address_book: AddressBook):  
->>>>>>> Stashed changes
     """Function fills up addres book with random contacts for debugging purposes"""
 
     random_contacts = [
         {'name': 'Zbyszek', 'last name': 'Kowalski', 'phone': '606505404', 'email': 'zbyszek.kowalski@gmail.com', 'birthday': '20 5 1990'},
-<<<<<<< Updated upstream
-        {'name': 'Rychu', 'last name': 'Nowak', 'phone': '546859652', 'email': 'rychu.nowak@gmail.com', 'birthday': '10 11 1995'}
-=======
         {'name': 'Rychu', 'last name': 'Nowak', 'phone': '546859652', 'email': 'rychu.nowak@gmail.com', 'birthday': '10 11 1995'},
         #{'name': 'Jan', 'last name': 'Wójcik', 'phone': '524835658', 'email': 'jan.wójcik@gmail.com', 'birthday': '5 9 1965'},
         # {'name': 'Adam', 'last name': 'Kowalczyk', 'phone': '044175272', 'email': 'adam.kowalczyk@yahoo.com', 'birthday': '20 9 1985'},
@@ -134,21 +158,14 @@ def test_contacts(address_book: AddressBook):
         # {'name': 'Jan', 'last name': 'Kamiński', 'phone': '818690456', 'email': 'jan.kamiński@yahoo.com', 'birthday': '20 7 1995'},
         # {'name': 'Jan', 'last name': 'Kowalski', 'phone': '441151946', 'email': 'jan.kowalski@gmail.com', 'birthday': '30 3 1986'},
         # {'name': 'Piotr', 'last name': 'Kowalczyk', 'phone': '506499840', 'email': 'piotr.kowalczyk@outlook.com', 'birthday': '16 5 1962'}
->>>>>>> Stashed changes
         ]
     
-    for person in random_contacts: # add random contacts to
+    for person in random_contacts:
         address_book.add_contact(person['name'], person['last name'])
     
     for contact_name in address_book.contacts:
         print(f'Name: {address_book.contacts[contact_name].name.value}')
         print(f'Last Name: {address_book.contacts[contact_name].last_name.value}')
-<<<<<<< Updated upstream
-
-def main():
-    test_contacts()    
-
-=======
 
 def end_program():
     print('Good bye')
@@ -185,6 +202,5 @@ def main():
         function_to_execute = input_parser()
         function_to_execute()
  
->>>>>>> Stashed changes
 if __name__ == '__main__':
     main()
