034049ba045d4990c6ed24716c38940f25496373
MrGegi <145406430+MrGegi@users.noreply.github.com>
2024-03-02 19:21:59 +0100

    help update and cleanup

--- main.py
+++ main.py
@@ -4,32 +4,6 @@ from sorting_module import main_sorting_folder
 import pickle
 import os
 
-#tu raczej powinno być command.keys() - nie trzeba wówczas ręcznie przepisywać funkcji
-available_commands = '''
-    add contact
-    delete_contact
-    add phone
-    change phone
-    delete phone
-    add email
-    change email
-    delete email
-    add birthday
-    birthday
-    add address
-    change address
-    delete address
-    add note
-    show notes
-    edit note
-    remove note
-    show all
-    find contact
-    save
-    exit
-    help
-'''
-
 address_book = load_from_file()
 
 LOGO = """
@@ -65,15 +39,15 @@ def test_contacts(address_book: AddressBook):
     """Function fills up addres book with random contacts for debugging purposes"""
 
     random_contacts = [
-        {'name': 'adam wójcik', 'phone': '206947535', 'email': 'adam.wojcik@yahoo.com', 'birthday': '1981-3-2', 'address': 'sosnowiec ladna 57/10'},
+        {'name': 'adam wojcik', 'phone': '206947535', 'email': 'adam.wojcik@yahoo.com', 'birthday': '1981-3-2', 'address': 'sosnowiec ladna 57/10'},
         {'name': 'tomasz kowalczyk', 'phone': '521861174', 'email': 'tomasz.kowalczyk@outlook.com', 'birthday': '1966-3-3', 'address': 'sosnowiec sloneczna 57/10'},
         {'name': 'jan kowalczyk', 'phone': None, 'email': 'jan.kowalczyk@outlook.com', 'birthday': '1960-3-11', 'address': 'radom polna 57/10'},
         {'name': 'andrzej kowalski', 'phone': '065754408', 'email': None, 'birthday': '1983-11-21', 'address': 'warszawa ladna 57/10'},
         {'name': 'tomasz nowak', 'phone': '046952319', 'email': 'tomasz.nowak@yahoo.com', 'birthday': None, 'address': 'sosnowiec kolorowa 57/10'},
-        {'name': 'adam kamiński', 'phone': '659435166', 'email': 'adam.kaminski@gmail.com', 'birthday': '1968-3-26', 'address': None},
+        {'name': 'adam kaminski', 'phone': '659435166', 'email': 'adam.kaminski@gmail.com', 'birthday': '1968-3-26', 'address': None},
         {'name': 'piotr nowak', 'phone': '678910629', 'email': 'piotr.nowak@yahoo.com', 'birthday': '1971-3-24', 'address': 'poznan 13/3'},
         {'name': 'adam kowalski', 'phone': '324844741', 'email': 'adam.kowalski@outlook.com', 'birthday': '1991-3-4', 'address': 'krakow 26/10'},
-        {'name': 'piotr kamiński', 'phone': '453802562', 'email': 'adam.kaminski@yahoo.com', 'birthday': '1980-9-22', 'address': 'warszawa 76/18'},
+        {'name': 'piotr kaminski', 'phone': '453802562', 'email': 'adam.kaminski@yahoo.com', 'birthday': '1980-9-22', 'address': 'warszawa 76/18'},
         {'name': 'tomasz adamek', 'phone': '164015881', 'email': 'tomasz.kowalczyk@outlook.com', 'birthday': '2002-1-3', 'address': 'sosnowiec 74/10'},
         {'name': 'piotr kowalczyk', 'phone': '915881994', 'email': 'jan.kowalczyk@yahoo.com', 'birthday': '1964-3-23', 'address': 'sosnowiec 100/11'},
         {'name': 'andrzej nowak', 'phone': '529363681', 'email': 'andrzej.nowak@gmail.com', 'birthday': '1999-2-8', 'address': 'krakow 13/1'},
@@ -82,7 +56,7 @@ def test_contacts(address_book: AddressBook):
         {'name': 'tomasz siekierka', 'phone': '623143519', 'email': 'tomasz.kowalczyk@gmail.com', 'birthday': '1977-12-6', 'address': 'sosnowiec 37/19'},
         {'name': 'jan kowalski', 'phone': '483958195', 'email': 'jan.kowalski@outlook.com', 'birthday': '1972-4-17', 'address': 'krakow 84/10'},
         {'name': 'leszek kowalski', 'phone': '039342693', 'email': 'jan.kowalski@outlook.com', 'birthday': '1981-5-3', 'address': 'poznan 97/17'},
-        {'name': 'andrzej kamiński', 'phone': '718624235', 'email': 'andrzej.kaminski@yahoo.com', 'birthday': '1973-1-14', 'address': 'gdansk 42/6'},
+        {'name': 'andrzej kaminski', 'phone': '718624235', 'email': 'andrzej.kaminski@yahoo.com', 'birthday': '1973-1-14', 'address': 'gdansk 42/6'},
         {'name': 'tomasz socha', 'phone': '052130035', 'email': 'tomasz.kowalczyk@gmail.com', 'birthday': '1964-8-14', 'address': 'krakow 17/6'},
         {'name': 'andrzej kowalczyk', 'phone': '087189332', 'email': 'andrzej.kowalczyk@outlook.com', 'birthday': '1967-3-27', 'address': 'gdansk 11/1'},
     ]
@@ -93,13 +67,26 @@ def test_contacts(address_book: AddressBook):
         address_book.contacts[person['name']].add_email(person['email'])
         address_book.contacts[person['name']].add_birthday(person['birthday'])
         address_book.contacts[person['name']].add_address(person['address'])
+    
+def add_contact():    
+    name = input("Enter the contact's name and surname: ")
+    if name in address_book.contacts:
+        print("A contact with this name already exists.")
+    else:
+        try:
+            address_book.contacts[name] = Contact(name)
+        except Exception as e:
+            print(e)
+        if name in address_book.contacts:
+                print(f"Contact {name} was added.")
 
-    # for contact_name in address_book.contacts:
-    #     print(f'Name: {address_book.contacts[contact_name].name.value}', end="  ")
-    #     print(f'Phone: {address_book.contacts[contact_name].phone.value}', end="  ")
-    #     print(f'Email: {address_book.contacts[contact_name].email.value}', end="  ")
-    #     print(f'Birthday: {address_book.contacts[contact_name].birthday.value}')
-      
+def delete_contact():
+    name = input("Enter the contact's name and surname you'd like to delete: ")
+    if name in address_book.contacts:
+        address_book.contacts.pop(name)
+        print(f'Contact {name} deleted.')
+    else:
+        print(f'There is no contact {name}')
 
 def add_phone():
     name = input("Enter the contact's name and surename: ")
@@ -125,24 +112,6 @@ def change_phone_num():
     else:
         print("Contact not found.")
 
-def find_contact():
-    contact_name = input("Enter the contact's name and surename: ")
-    if contact_name in address_book.contacts:
-        width = 154
-        print("\n+" + "-" * width + "+")
-        print('|{:^30}|{:^30}|{:^30}|{:^30}|{:^30}|'.format("NAME", "PHONE", "EMAIL", "BIRTHDAY", "ADDRESS"))
-        print("+" + "-" * width + "+")
-        contact = address_book.contacts[contact_name]
-        format_value = lambda x: x if x is not None else "---"
-        print('|{:^30}'.format(format_value(contact.name.value.title())), end="")
-        print('|{:^30}'.format(format_value(contact.phone.value)), end="")
-        print('|{:^30}'.format(format_value(contact.email.value)), end="")
-        print('|{:^30}'.format(format_value(contact.birthday.value)), end="")
-        print('|{:^30}|'.format(format_value(contact.address.value)), end="\n")
-        print("+" + "-" * width + "+\n")
-    else:
-        print("Contact not found.")
-
 def delete_phone():
     name = input("Enter the contact's name and surename: ")
     if name in address_book.contacts:
@@ -151,39 +120,41 @@ def delete_phone():
     else:
         print("Contact not found.")
 
-def save_to_file():
-    with open('bot_save.bin', "wb") as fh:
-        pickle.dump(address_book, fh)
-    print('File has been saved')
-
-def end_program():
-    # save_to_file()
-    print('Goodbye')
-
-def add_contact():    
+def add_email():
     name = input("Enter the contact's name and surname: ")
-    if name in address_book.contacts:
-        print("A contact with this name already exists.")
-    else:
-        try:
-            address_book.contacts[name] = Contact(name)
-        except Exception as e:
-            print(e)
-        if name in address_book.contacts:
-                print(f"Contact {name} was added.")
-
-def delete_contact():
-    name = input("Enter the contact's name and surname you'd like to delete: ")
-    if name in address_book.contacts:
-        address_book.contacts.pop(name)
-        print(f'Contact {name} deleted.')
-    else:
+    if not name in address_book.contacts:
         print(f'There is no contact {name}')
-
-def unknown_command(commands: dict):
-    print("\nUnknown command! Please choose one from the list provided below:\n")
-    for command in commands.keys():
-        print(f"   {command}")
+        return
+    email = input("Enter the email: ")
+    try:
+        address_book.contacts[name].add_email(email)
+        if address_book.contacts[name].email.value:
+            print(f"{email} was added to contact {name}.")
+    except:
+        return
+    
+def change_email():
+    name = input("Enter the contact's name and surname: ")
+    if not name in address_book.contacts:
+        print(f'There is no contact {name}')
+        return
+    if not address_book.contacts[name].email.value:
+        print(f"Contact {name} doesnt have a email yet. However we can proceed.")
+    email = input("Enter the email: ")
+    try:
+        address_book.contacts[name].add_email(email)
+        if address_book.contacts[name].email.value == email:
+            print(f"{email} was changed for contact {name}.")
+    except:
+        return
+    
+def delete_email():
+    name = input("Enter the contact's name and surname: ")
+    if not name in address_book.contacts:
+        print(f'There is no contact {name}')
+        return
+    address_book.contacts[name].remove_email()
+    print(f'Email deleted')
 
 def set_birthday():
     name = input("Enter the contact's name and surename: ").lower()
@@ -254,57 +225,6 @@ def days_to_birthday():
             
     print("+" + "-" * width + "+\n")
 
-def show_all():
-    width = 154
-    print("\n+" + "-" * width + "+")
-    print('|{:^30}|{:^30}|{:^30}|{:^30}|{:^30}|'.format("NAME", "PHONE", "EMAIL", "BIRTHDAY", "ADDRESS"))
-    print("+" + "-" * width + "+")
-    for contact_name in address_book.contacts:
-        contact = address_book.contacts[contact_name]
-        format_value = lambda x: x if x is not None else "---"
-        print('|{:^30}'.format(format_value(contact.name.value.title())), end="")
-        print('|{:^30}'.format(format_value(contact.phone.value)), end="")
-        print('|{:^30}'.format(format_value(contact.email.value)), end="")
-        print('|{:^30}'.format(format_value(contact.birthday.value)), end="")
-        print('|{:^30}|'.format(format_value(contact.address.value)), end="\n")
-    print("+" + "-" * width + "+\n")
-
-def add_email():
-    name = input("Enter the contact's name and surname: ")
-    if not name in address_book.contacts:
-        print(f'There is no contact {name}')
-        return
-    email = input("Enter the email: ")
-    try:
-        address_book.contacts[name].add_email(email)
-        if address_book.contacts[name].email.value:
-            print(f"{email} was added to contact {name}.")
-    except:
-        return
-    
-def change_email():
-    name = input("Enter the contact's name and surname: ")
-    if not name in address_book.contacts:
-        print(f'There is no contact {name}')
-        return
-    if not address_book.contacts[name].email.value:
-        print(f"Contact {name} doesnt have a email yet. However we can proceed.")
-    email = input("Enter the email: ")
-    try:
-        address_book.contacts[name].add_email(email)
-        if address_book.contacts[name].email.value == email:
-            print(f"{email} was changed for contact {name}.")
-    except:
-        return
-    
-def delete_email():
-    name = input("Enter the contact's name and surname: ")
-    if not name in address_book.contacts:
-        print(f'There is no contact {name}')
-        return
-    address_book.contacts[name].remove_email()
-    print(f'Email deleted')
-
 def add_address():
     name = input("Enter the contact's name and surname: ")
     if not name in address_book.contacts:
@@ -347,8 +267,10 @@ def delete_address():
     address_book.contacts[name].remove_address()
     print(f'Address deleted')
 
-def show_notes():
-    print('List of notes: \n', address_book.notebook.show_notes())
+def add_note():
+    note = input("Enter the note text: ")
+    tags = input("Enter tags: ")
+    address_book.notebook.add_note(note, tags)
 
 def edit_note():
     if not address_book.notebook.data:
@@ -366,14 +288,45 @@ def remove_note():
         num_of_note = input('Enter number of note or write "all" to remove all notes: ')
         address_book.notebook.remove_note(num_of_note)
 
-def add_note():
-    note = input("Enter the note text: ")
-    tags = input("Enter tags: ")
-    address_book.notebook.add_note(note, tags)
+def show_notes():
+    print('List of notes: \n', address_book.notebook.show_notes())
 
 def search_note_by_tags():
     searched_tags = input("Enter tags: ")
     print(address_book.notebook.search_note_by_tags(searched_tags))
+
+def show_all():
+    width = 154
+    print("\n+" + "-" * width + "+")
+    print('|{:^30}|{:^30}|{:^30}|{:^30}|{:^30}|'.format("NAME", "PHONE", "EMAIL", "BIRTHDAY", "ADDRESS"))
+    print("+" + "-" * width + "+")
+    for contact_name in address_book.contacts:
+        contact = address_book.contacts[contact_name]
+        format_value = lambda x: x if x is not None else "---"
+        print('|{:^30}'.format(format_value(contact.name.value.title())), end="")
+        print('|{:^30}'.format(format_value(contact.phone.value)), end="")
+        print('|{:^30}'.format(format_value(contact.email.value)), end="")
+        print('|{:^30}'.format(format_value(contact.birthday.value)), end="")
+        print('|{:^30}|'.format(format_value(contact.address.value)), end="\n")
+    print("+" + "-" * width + "+\n")
+
+def find_contact():
+    contact_name = input("Enter the contact's name and surename: ")
+    if contact_name in address_book.contacts:
+        width = 154
+        print("\n+" + "-" * width + "+")
+        print('|{:^30}|{:^30}|{:^30}|{:^30}|{:^30}|'.format("NAME", "PHONE", "EMAIL", "BIRTHDAY", "ADDRESS"))
+        print("+" + "-" * width + "+")
+        contact = address_book.contacts[contact_name]
+        format_value = lambda x: x if x is not None else "---"
+        print('|{:^30}'.format(format_value(contact.name.value.title())), end="")
+        print('|{:^30}'.format(format_value(contact.phone.value)), end="")
+        print('|{:^30}'.format(format_value(contact.email.value)), end="")
+        print('|{:^30}'.format(format_value(contact.birthday.value)), end="")
+        print('|{:^30}|'.format(format_value(contact.address.value)), end="\n")
+        print("+" + "-" * width + "+\n")
+    else:
+        print("Contact not found.")
 
 def sort_folder():
     current_path = os.getcwd()
@@ -381,9 +334,23 @@ def sort_folder():
     main_sorting_folder(path_to_folder)
     os.chdir(current_path)
 
-def accepted_commands():
-    print(available_commands)
+def save_to_file():
+    with open('bot_save.bin', "wb") as fh:
+        pickle.dump(address_book, fh)
+    print('File has been saved')
 
+def end_program():
+    save_to_file()
+    print('Goodbye')
+
+def accepted_commands(commands):
+    for command in commands.keys():
+        print(f"   {command}")
+
+def unknown_command(commands: dict):
+    print("\nUnknown command! Please choose one from the list provided below:\n")
+    for command in commands.keys():
+        print(f"   {command}")
 
 def input_parser():
     """Functions runs in a while loop, takes input from user and returns apropiate functions
@@ -391,36 +358,33 @@ def input_parser():
     commands = {
     'add contact': add_contact,
     'delete contact': delete_contact,
-    'add note': add_note,
-    'show notes':show_notes,
-    'search note': search_note_by_tags,
-    'edit note': edit_note,
-    'remove note': remove_note,
     'add phone': add_phone,
     'change phone': change_phone_num,
     'delete phone': delete_phone,
-    'add birthday' : set_birthday,
-    'birthday': days_to_birthday,
     'add email': add_email,
     'change email': change_email,
     'delete email': delete_email,
+    'add birthday' : set_birthday,
+    'birthday': days_to_birthday,
     'add address': add_address,
     'change address': change_address,
     'delete address': delete_address,
+    'add note': add_note,
+    'edit note': edit_note,
+    'remove note': remove_note,
+    'show notes':show_notes,
+    'search note': search_note_by_tags,
     'show all': show_all,
     'find contact' : find_contact,
     'sort folder': sort_folder,
     'save': save_to_file,
     'exit': end_program,
-    'end': end_program,
-    '.': end_program,
     'help': accepted_commands, 
-
-}
-    
+}   
     command = input('\nEnter your command: ').lower()
-
-    if command in commands:
+    if command == 'help':
+        return accepted_commands(commands)
+    elif command in commands:
         return commands[command]  
     else:
         return unknown_command(commands)
@@ -439,7 +403,6 @@ def main():
                 function_to_execute()
         except:
             continue
-           
- 
+            
 if __name__ == '__main__':
     main()
