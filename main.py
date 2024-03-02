eb57b4292e3a93da4a8ba9a79de84f0658725634
Vincenty82 <vincenty82@gmail.com>
2024-03-02 11:14:09 +0100

    modified show all

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def test_contacts(address_book: AddressBook):
     """Function fills up addres book with random contacts for debugging purposes"""
 
     random_contacts = [
+        # {'name': 'NAME', 'phone': 'PHONE', 'email': 'EMAIL', 'birthday': 'BIRTHDAY', "address": "ADDRESS"},
         {'name': 'zbyszek kowalski', 'phone': '606505404', 'email': 'zbyszek.kowalski@gmail.com', 'birthday': '1990-5-20'},
         {'name': 'rychu nowak', 'phone': '546859652', 'email': 'rychu.nowak@gmail.com', 'birthday': None},
         {'name': 'adam nowak', 'phone': None, 'email': 'adam.nowak@outlook.com', 'birthday': '1973-1-26'},
@@ -58,11 +59,11 @@ def test_contacts(address_book: AddressBook):
         address_book.contacts[person['name']].add_email(person['email'])
         address_book.contacts[person['name']].add_birthday(person['birthday'])
 
-    for contact_name in address_book.contacts:
-        print(f'Name: {address_book.contacts[contact_name].name.value}', end="  ")
-        print(f'Phone: {address_book.contacts[contact_name].phone.value}', end="  ")
-        print(f'Email: {address_book.contacts[contact_name].email.value}', end="  ")
-        print(f'Birthday: {address_book.contacts[contact_name].birthday.value}')
+    # for contact_name in address_book.contacts:
+    #     print(f'Name: {address_book.contacts[contact_name].name.value}', end="  ")
+    #     print(f'Phone: {address_book.contacts[contact_name].phone.value}', end="  ")
+    #     print(f'Email: {address_book.contacts[contact_name].email.value}', end="  ")
+    #     print(f'Birthday: {address_book.contacts[contact_name].birthday.value}')
       
 
 def add_phone():
@@ -180,11 +181,19 @@ def days_to_birthday():
     return
 
 def show_all():
-        for contact_name in address_book.contacts:
-            print(f'Name: {address_book.contacts[contact_name].name.value}', end="  ")
-            print(f'Phone: {address_book.contacts[contact_name].phone.value}', end="  ")
-            print(f'Birthday: {address_book.contacts[contact_name].birthday.value}', end="  ")
-            print(f'Email: {address_book.contacts[contact_name].email.value}')
+    width = 154
+    print("\n+" + "-" * width + "+")
+    print('|{:^30}|{:^30}|{:^30}|{:^30}|{:^30}|'.format("NAME", "PHONE", "EMAIL", "BIRHDAY", "ADDRESS"))
+    print("+" + "-" * width + "+")
+    for contact_name in address_book.contacts:
+        contact = address_book.contacts[contact_name]
+        format_value = lambda x: x if x is not None else "---"
+        print('|{:^30}'.format(format_value(contact.name.value)), end="")
+        print('|{:^30}'.format(format_value(contact.phone.value)), end="")
+        print('|{:^30}'.format(format_value(contact.email.value)), end="")
+        print('|{:^30}'.format(format_value(contact.birthday.value)), end="")
+        print('|{:^30}|'.format(format_value(contact.address.value)), end="\n")
+    print("+" + "-" * width + "+\n")
 
 def add_email():
     name = input("Enter the contact's name and surname: ")
