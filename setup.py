cd10db20542fe4619788446165e0b421b3d329e8
MrGegi <145406430+MrGegi@users.noreply.github.com>
2024-03-03 21:36:14 +0100

    setup change

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='Osobisty_asystent',
     version='1.0',
-    packages=['address_book'],
+    packages=find_namespace_packages(),
     entry_points={
         'console_scripts': [
             'personal-assistant = address_book.main:main'
