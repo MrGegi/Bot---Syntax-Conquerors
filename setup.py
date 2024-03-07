ec37b804089c9155dfe42368b41f74ad20e56985
Vincenty82 <143327758+Vincenty82@users.noreply.github.com>
2024-03-07 16:52:31 +0100

    ver. 1.0.0.1 - package .json support

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
 setup(
     name='Osobisty_asystent',
-    version='1.2',
+    version='1.0.0.1',
     packages=['address_book'],
-    entry_points={
-        'console_scripts': [
-            'personal-assistant = address_book.assistant:main'
-        ]
-    }
+    entry_points={'console_scripts': ['personal-assistant = address_book.assistant:main']},
+    package_data={'address_book': ["*.json"]},
 )
